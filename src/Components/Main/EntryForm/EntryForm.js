45c3cb380a6a9dc617da861108f3687476f3e6c9
Fredrik Wictorsson <wictorsson@gmail.com>
2023-03-16 16:46:49 +0100

    Add basic structure

---
+++ src/Components/Main/EntryForm/EntryForm.js
@@ -0,0 +1,3 @@
+export default function EntryForm() {
+  return <div>EntryForm</div>;
+}
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,10 @@
+import EntryForm from "./EntryForm/EntryForm";
+import EntriesSection from "./EntriesSection/EntriesSection";
+export default function Main() {
+  return (
+    <div>
+      (Main) New Entry<EntryForm></EntryForm>
+      <EntriesSection></EntriesSection>
+    </div>
+  );
+}
